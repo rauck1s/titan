68fe14e6e2e24af1cc2489c6782d918fc31acf8e
Dmitri Minckevich <rauckees0811@gmai.com>
2025-02-16 19:22:02 +0300

    update: update app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { CourseModule } from './course/course.module';
+import { NotificationModule } from './notification/notification.module';
+import { TestsController } from './tests/tests.controller';
+import { TestsModule } from './tests/tests.module';
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController],
+  imports: [UserModule, CourseModule, NotificationModule, TestsModule],
+  controllers: [AppController, TestsController],
   providers: [AppService],
 })
 export class AppModule {}
